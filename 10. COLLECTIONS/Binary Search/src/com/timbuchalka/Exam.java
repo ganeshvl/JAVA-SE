7d8463541ddcc60cc8034e8339b6408167c9ca32
Ebenezer Chinyati <echinyati@live.com>
2019-10-29 14:09:32 +0000

    Add files via upload

---
+++ 10. COLLECTIONS/Binary Search/src/com/timbuchalka/Exam.java
@@ -0,0 +1,14 @@
+package com.timbuchalka;
+
+public class Exam {
+
+    public static final Integer Kudzi = -50;
+
+    public static void main(String args[]) {
+
+                int a = Integer.MIN_VALUE;
+                int b = -a;
+                System.out.println( a+ "   "+b);
+            }
+        }
+
